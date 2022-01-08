42006da59de673963735de877b2931c169c61849
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-08 00:20:20 -0300

    first commit

---
+++ src/main/java/br/com/java/javabeanvalidation/JavabeanvalidationApplication.java
@@ -0,0 +1,12 @@
+package br.com.java.javabeanvalidation;
+
+
+
+
+public class JavabeanvalidationApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Iniciando...");
+	}
+
+}
---
+++ src/test/java/br/com/java/javabeanvalidation/JavabeanvalidationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.javabeanvalidation;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class JavabeanvalidationApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
